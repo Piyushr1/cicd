f4b96d2403f58c1568c6683fc610adbdc3f25c73
Piyushr1 <45657730+Piyushr1@users.noreply.github.com>
2024-03-17 17:36:35 +0530

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -2,7 +2,8 @@
 import json
 
 def lambda_handler(event, context):
-    # Simulate processing sales data contained in the event
+    print("This log is a new addition to verify deployment.")
+    # Assume this is your modified data processing logic
     print("Calculating total sales...")
     
     total_sales = sum(item['amount'] for item in event['sales'])
